82c7dad8c36196bb09d11198b5c1e26d6ef5fa35
9wynneth <gisviandhy@student.ciputra.ac.id>
2023-10-15 10:26:55 +0700

    Initial Commit

---
+++ W5_Flag Quiz Game/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  W5_Flag Quiz Game
+//
+//  Created by MacBook Pro on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ W5_Flag Quiz Game/W5_Flag_Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  W5_Flag_Quiz_GameApp.swift
+//  W5_Flag Quiz Game
+//
+//  Created by MacBook Pro on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct W5_Flag_Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
